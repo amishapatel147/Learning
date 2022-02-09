aea20c6173ddc925efca99784e08e75e9ac3d109
amishapatel <amishap@zignuts.com>
2022-02-09 18:47:41 +0530

    lets try

---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Two way data binding</h1>
+        enter somethings:<input type="text" v-model="name"/>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'home',
+    data()
+    {
+        return{
+            name:'amisha'
+        }
+    }
+}
+</script>
+<style scoped>
+
+h1{
+
+color:orange;
+}
+
+</style>
